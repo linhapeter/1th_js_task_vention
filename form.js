3aec336e3835d06f45d06bd4bc3bcb235ff22d0a
linhapeter <linha.peter.jr@gmail.com>
2023-09-20 10:53:32 +0200

    Added functions for creating elements with attributes and appending them

--- form.js
+++ form.js
@@ -1,23 +1,37 @@
-const form = document.createElement('form');
+function createElement(tag, attributes = {}) {
+    const element = document.createElement(tag);
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+    console.log(element);
+    return element;
+}
+
+
+function appendChildToParent(parent, child) {
+    parent.appendChild(child);
+}
+
+
+const form = createElement('form');
+
 
 const inputFields = [
-    { type: 'email', name: 'email', palaceholder: 'Email' },
-    { type: 'password', name: 'password', palaceholder: 'Password' }
+    { type: 'email', name: 'email', placeholder: 'Email' },
+    { type: 'password', name: 'password', placeholder: 'Password' }
 ];
 
+
 inputFields.forEach(field => {
-    let input = document.createElement('input');
-    input.setAttribute('type', field.type);
-    input.setAttribute('name', field.name);
-    input.setAttribute('placeholder', field.palaceholder);
-    form.appendChild(input);
+    const input = createElement('input', { type: field.type, name: field.name, placeholder: field.placeholder });
+    appendChildToParent(form, input);
 });
 
 
-const submitButton = document.createElement('button');
-submitButton.setAttribute('type', 'submit');
+const submitButton = createElement('button', { type: 'submit' });
 submitButton.textContent = 'Submit';
-form.appendChild(submitButton);
+appendChildToParent(form, submitButton);
+
 
 const rootDiv = document.getElementById('root');
-rootDiv.appendChild(form);
+appendChildToParent(rootDiv, form);
