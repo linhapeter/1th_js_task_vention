03fad32161b869477be7c0258a2858c12443cf78
linhapeter <linha.peter.jr@gmail.com>
2023-09-20 12:21:27 +0200

    Event listener added to form

--- form.js
+++ form.js
@@ -12,6 +12,7 @@ passwordInput.setAttribute('placeholder', 'Password');
 
 const submitButton = document.createElement('button');
 submitButton.setAttribute('type', 'submit');
+submitButton.setE
 submitButton.textContent = 'Submit';
 
 form.appendChild(emailInput);
@@ -20,3 +21,19 @@ form.appendChild(submitButton);
 
 const rootDiv = document.getElementById('root');
 rootDiv.appendChild(form);
+
+
+const existingForm = document.querySelector('form');
+
+
+existingForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+
+    const emailInput = existingForm.querySelector('input[name="email"]');
+    const passwordInput = existingForm.querySelector('input[name="password"]');
+
+
+    console.log('Email:', emailInput.value);
+    console.log('Password:', passwordInput.value);
+});
