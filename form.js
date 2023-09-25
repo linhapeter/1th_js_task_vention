a1f337094ff295384451671240e528dbdb825f47
igor mikailov <igormikailov@gmail.com>
2023-09-25 22:22:43 +0200

    feature/11 - fix code style

--- form.js
+++ form.js
@@ -1,81 +1,72 @@
 const formData = new FormData();
 
 const createNewElement = (tag) => {
-    return document.createElement(tag);
+  return document.createElement(tag);
 };
-
 
 const setAttributesForElement = (element, attributes = {}) => {
-    for (const key in attributes) {
-        element.setAttribute(key, attributes[key]);
-    }
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
 };
-
 
 const appendChildToParent = (parent, child) => {
-    parent.appendChild(child);
+  parent.appendChild(child);
 };
-
 
 const form = createNewElement('form');
 
-
 const inputFields = [
-    { type: 'email', name: 'email', placeholder: 'Email' },
-    { type: 'password', name: 'password', placeholder: 'Password' }
+  { type: 'email', name: 'email', placeholder: 'Email' },
+  { type: 'password', name: 'password', placeholder: 'Password' },
 ];
 
+const submitForm = async (e) => {
+  e.preventDefault();
 
-const submitForm = async e => {
-    e.preventDefault();
+  const emailInput = form.querySelector('input[name="email"]');
+  const passwordInput = form.querySelector('input[name="password"]');
 
+  formData.append('email', emailInput.value);
+  formData.append('password', passwordInput.value);
 
-    const emailInput = form.querySelector('input[name="email"]');
-    const passwordInput = form.querySelector('input[name="password"]');
+  try {
+    const response = await fetch('https://httpbin.org/post', {
+      method: 'POST',
+      body: formData,
+    });
 
-
-    formData.append('email', emailInput.value);
-    formData.append('password', passwordInput.value);
-
-
-    try {
-        const response = await fetch('https://httpbin.org/post', {
-            method: 'POST',
-            body: formData
-        });
-
-        if (response.ok) {
-            const data = await response.json();
-            await showResponse(data.form);
-        } else {
-            console.error('Failed to send the request.');
-        }
-    } catch (error) {
-        console.error('An error occurred:', error);
+    if (response.ok) {
+      const data = await response.json();
+      await showResponse(data.form);
+    } else {
+      console.error('Failed to send the request.');
     }
-}
-
-
-const showResponse = async(data) => {
-    console.log(data);
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
 };
 
+const showResponse = async (data) => {
+  console.log(data);
+};
 
-inputFields.forEach(field => {
-    const input = createNewElement('input');
-    setAttributesForElement(input, { type: field.type, name: field.name, placeholder: field.placeholder });
-    appendChildToParent(form, input);
+inputFields.forEach((field) => {
+  const input = createNewElement('input');
+  setAttributesForElement(input, {
+    type: field.type,
+    name: field.name,
+    placeholder: field.placeholder,
+  });
+  appendChildToParent(form, input);
 });
-
 
 const submitButton = createNewElement('button');
 setAttributesForElement(submitButton, { type: 'submit' });
 submitButton.textContent = 'Submit';
 appendChildToParent(form, submitButton);
 
-
 const rootDiv = document.getElementById('root');
 rootDiv.appendChild(form);
-
 
 form.addEventListener('submit', submitForm);
