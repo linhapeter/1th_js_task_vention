108aa77d40a73436e681482540970e42ce419428
linhapeter <linha.peter.jr@gmail.com>
2023-10-02 10:47:49 +0200

    Initialized correctly

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,19 @@
-// webpack.config.js
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './form.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [{
             test: /\.js$/,
             exclude: /node_modules/,
             use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env'],
-                },
-            },
-        }, ],
-    },
+                loader: 'babel-loader'
+            }
+        }]
+    }
 };
