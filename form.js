cd9520e2fa9761f9d36bc6add5dd4a47465c820e
linhapeter <linha.peter.jr@gmail.com>
2023-10-04 10:08:53 +0200

    Added correct extension and indentation set on 2

--- form.js
+++ form.js
@@ -1,71 +1,71 @@
 import { createNewElement, setAttributesForElement, appendChildToParent, validateForm, saveInputValue, loadInputValues, deleteInputValue, createLoadingMsg, removeLoadingMsg } from './helpers.js';
 
 const submitForm = async e => {
-    e.preventDefault();
-    if (!validateForm(form.email.value, form.password.value)) return;
+  e.preventDefault();
+  if (!validateForm(form.email.value, form.password.value)) return;
 
-    const emailInput = form.querySelector('input[name="email"]');
-    const passwordInput = form.querySelector('input[name="password"]');
+  const emailInput = form.querySelector('input[name="email"]');
+  const passwordInput = form.querySelector('input[name="password"]');
 
-    formData.delete('email', emailInput.value);
-    formData.delete('password', passwordInput.value);
-    formData.append('email', emailInput.value);
-    formData.append('password', passwordInput.value);
+  formData.delete('email', emailInput.value);
+  formData.delete('password', passwordInput.value);
+  formData.append('email', emailInput.value);
+  formData.append('password', passwordInput.value);
 
-    createLoadingMsg(form);
+  createLoadingMsg(form);
 
-    try {
-        const response = await fetch('https://httpbin.org/post', {
-            method: 'POST',
-            body: formData
-        });
-        const data = await response.json();
+  try {
+    const response = await fetch('https://httpbin.org/post', {
+      method: 'POST',
+      body: formData
+    });
+    const data = await response.json();
 
-        removeLoadingMsg();
+    removeLoadingMsg();
 
-        addElementsWithOutputContent(data.form);
-        clearInputState(form);
-    } catch (error) {
-        console.error('An error occurred:', error);
-    }
+    addElementsWithOutputContent(data.form);
+    clearInputState(form);
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
 }
 
 const addElementsWithOutputContent = (data) => {
-    dataContainer.innerHTML = '';
-    for (const key in data) {
-        const keyValueElement = createNewElement('div');
-        keyValueElement.textContent = `${key}: ${data[key]}`;
-        dataContainer.appendChild(keyValueElement);
-    }
+  dataContainer.innerHTML = '';
+  for (const key in data) {
+    const keyValueElement = createNewElement('div');
+    keyValueElement.textContent = `${key}: ${data[key]}`;
+    dataContainer.appendChild(keyValueElement);
+  }
 }
 
 const saveInputState = () => {
-    const emailInput = form.querySelector('input[name="email"]');
-    const passwordInput = form.querySelector('input[name="password"]');
+  const emailInput = form.querySelector('input[name="email"]');
+  const passwordInput = form.querySelector('input[name="password"]');
 
-    saveInputValue(emailInput.value, passwordInput.value);
+  saveInputValue(emailInput.value, passwordInput.value);
 };
 
 const loadInputState = () => {
-    const emailInput = form.querySelector('input[name="email"]');
-    const passwordInput = form.querySelector('input[name="password"]');
-    const { savedEmail, savedPassword } = loadInputValues();
+  const emailInput = form.querySelector('input[name="email"]');
+  const passwordInput = form.querySelector('input[name="password"]');
+  const { savedEmail, savedPassword } = loadInputValues();
 
-    if (savedEmail !== null) {
-        emailInput.value = savedEmail;
-    }
+  if (savedEmail !== null) {
+    emailInput.value = savedEmail;
+  }
 
-    if (savedPassword !== null) {
-        passwordInput.value = savedPassword;
-    }
+  if (savedPassword !== null) {
+    passwordInput.value = savedPassword;
+  }
 };
 
 const clearInputState = (form) => {
-    form.querySelectorAll('input').forEach(input => {
-        input.value = '';
-    });
+  form.querySelectorAll('input').forEach(input => {
+    input.value = '';
+  });
 
-    deleteInputValue();
+  deleteInputValue();
 };
 
 const formData = new FormData();
@@ -73,18 +73,18 @@ const formData = new FormData();
 const form = createNewElement('form');
 
 const inputFields = [
-    { type: 'email', name: 'email', placeholder: 'Email' },
-    { type: 'password', name: 'password', placeholder: 'Password' }
+  { type: 'email', name: 'email', placeholder: 'Email' },
+  { type: 'password', name: 'password', placeholder: 'Password' }
 ];
 
 inputFields.forEach(field => {
-    const input = createNewElement('input');
-    setAttributesForElement(input, {
-        type: field.type,
-        name: field.name,
-        placeholder: field.placeholder,
-    });
-    appendChildToParent(form, input);
+  const input = createNewElement('input');
+  setAttributesForElement(input, {
+    type: field.type,
+    name: field.name,
+    placeholder: field.placeholder,
+  });
+  appendChildToParent(form, input);
 });
 
 const submitButton = createNewElement('button');
@@ -98,7 +98,7 @@ rootDiv.appendChild(form);
 document.addEventListener('DOMContentLoaded', loadInputState);
 
 form.querySelectorAll('input').forEach(input => {
-    input.addEventListener('input', saveInputState);
+  input.addEventListener('input', saveInputState);
 });
 
 const dataContainer = createNewElement('div');
